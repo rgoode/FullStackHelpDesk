8dbeab92e65b6936547dfd3c5c2cd3730a32200f
cstanescu123 <94859191+cstanescu123@users.noreply.github.com>
2022-04-26 13:09:06 -0400

    created entity database

---
+++ BackEndApi/Migrations/20220426170532_InitialCreate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackEndApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ClosedTickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RequestTitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Problem = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Solution = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Priority = table.Column<int>(type: "int", nullable: false),
+                    SubmittedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AssignedTo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Favorited = table.Column<bool>(type: "bit", nullable: false),
+                    ResolvedTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClosedTickets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OpenTickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RequestTitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Problem = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Solution = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Priority = table.Column<int>(type: "int", nullable: false),
+                    SubmittedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AssignedTo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Favorited = table.Column<bool>(type: "bit", nullable: false),
+                    ResolvedTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OpenTickets", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClosedTickets");
+
+            migrationBuilder.DropTable(
+                name: "OpenTickets");
+        }
+    }
+}
---
+++ BackEndApi/Services/DALModels/ClosedTicket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BackEndApi.Models.Ticket
+{
+    public class ClosedTicket
+    {
+        public int Id { get; set; }
+        public string RequestTitle { get; set; }
+        public string Name { get; set; }
+        public string Problem { get; set; }
+        public string Solution { get; set; }
+        public int Priority { get; set; }
+        public DateTime SubmittedTime { get; set; }
+        public string AssignedTo { get; set; }
+        public bool Favorited { get; set; }
+        public DateTime ResolvedTime { get; set; }
+
+    }
+}
