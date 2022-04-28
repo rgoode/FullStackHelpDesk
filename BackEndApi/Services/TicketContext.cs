c01c2fe6fdf04e4df4cd04e41d64b9968cae68c7
Kyle McIntosh <krmcintosh87@gmail.com>
2022-04-27 21:51:28 -0400

    adding controllers and mthods for the stuff and things

--- BackEndApi/Services/TicketContext.cs
+++ BackEndApi/Services/TicketContext.cs
@@ -1,13 +1,43 @@
 ﻿using BackEndApi.Models.Ticket;
 using BackEndApi.Services.DALModels;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace BackEndApi.Services
 {
-    public class TicketContext : DbContext, ITicketContext
+    public class TicketContext : DbContext, ITicketContext, IDeleteTicket
     {
         public DbSet<Tickets> Tickets { get; set; }
         public DbSet<Users> Users { get; set; }
+
+        public IEnumerable<Tickets> GetTickets()
+        {
+            return Tickets;
+        }
+
+        public Tickets GetTicket(int ticketId)
+        {
+            var dbTickets= Tickets.Find(ticketId);
+
+            return dbTickets;
+        }
+
+        public Tickets DeleteTicket(int ticketId)
+        {
+            var dbTickets = Tickets.Find(ticketId);
+
+            if (dbTickets != null)
+            {
+                var entity = Tickets.Remove(dbTickets).Entity;
+                SaveChanges();
+                return entity;
+            }
+            return null;
+        }
+
+
+
+
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
