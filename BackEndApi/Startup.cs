b7c506267ef1039541ffe6bf6ce3ee02a3324cf7
Kyle McIntosh <krmcintosh87@gmail.com>
2022-04-27 20:38:10 -0400

    bringing models back...

--- BackEndApi/Startup.cs
+++ BackEndApi/Startup.cs
@@ -1,10 +1,11 @@
-using BackEndApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
@@ -25,7 +26,6 @@ namespace BackEndApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ITicketContext, TicketContext>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
