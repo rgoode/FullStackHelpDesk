f6195546574f02a21ea02ca58598abda9204ddbd
Rachel Goode <rgoode92@gmail.com>
2022-05-02 19:47:33 -0400

    forms updated

--- Help-Desk-Angular/src/app/users.service.ts
+++ Help-Desk-Angular/src/app/users.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Tickets } from './models/Tickets';
+import { PutTicket } from './models/User';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+  baseUrl = "https://localhost:5001/Tickets";
+
+
+
+  putTicket(user: PutTicket) {
+    return this.httpClient.put<Tickets>(this.baseUrl, user);
+  }
 }
