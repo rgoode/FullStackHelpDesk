ede1de12d85a23fcc88ea5fa2f8be935c1f66d76
Kyle McIntosh <krmcintosh87@gmail.com>
2022-05-03 18:55:36 -0400

    updated angular models

--- Help-Desk-Angular/src/app/models/Tickets.ts
+++ Help-Desk-Angular/src/app/models/Tickets.ts
@@ -1,11 +1,27 @@
-export interface Tickets{
-    ticketId: number;
-    submitterName: string;
-    submitterEmail: string;
-    subject: string;
-    problem: string;
-    solution: string;
+// export interface Tickets{
+//     ticketId: number;
+//     submitterName: string;
+//     submitterEmail: string;
+//     subject: string;
+//     problem: string;
+//     solution: string;
+// }
 
-}
+export interface Tickets {
+    ticketID: number
+    submitterName: string
+    submitterEmail: string
+    subjectLine: string
+    problem: string
+    submittedTime: string
+    solution: string
+    priority: number
+    assignedTo: string
+    status: string
+    resolvedTime: string
+    users: any
+  }
+
+
 
 export type PostTicket = Omit<Tickets, "ticketId">;
